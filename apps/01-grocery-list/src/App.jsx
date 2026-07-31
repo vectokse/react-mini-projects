c33c7faad13120914aaf5fd490ec4ea8cb51d10e
Vectokse <vectokse.dev@gmail.com>
2026-07-31 17:57:58 +0200

    🚧 Grocery > add ProductCard component

--- apps/01-grocery-list/src/App.jsx
+++ apps/01-grocery-list/src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import ListSummary from "./components/ListSummary";
+import ProductCard from "./components/ProductCard";
 
 function App() {
+  const [checked, setChecked] = useState(false);
+
+  const toogleProduct = () => {
+    setChecked(!checked);
+  };
+
+  const deleteProduct = () => {
+    alert("delete Product");
+  };
+
   return (
     <>
       <ListSummary />
+      <ProductCard
+        name={"chocolat"}
+        isCheck={checked}
+        onToggle={toogleProduct}
+        onDelete={deleteProduct}
+      />
     </>
   );
 }
