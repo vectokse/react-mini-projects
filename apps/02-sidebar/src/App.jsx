9dbb53c89960eacf1ced6533ce6519b24800fab0
Vectokse <vectokse.dev@gmail.com>
2026-08-03 11:48:07 +0200

    🚧 Sidebar > add MenuItem component

--- apps/02-sidebar/src/App.jsx
+++ apps/02-sidebar/src/App.jsx
@@ -1,7 +1,16 @@
+import MenuItem from "./components/MenuItem";
+import { MdOutlineDashboard } from "react-icons/md";
+
 function App() {
   return (
     <>
       <p>sidebar</p>
+      <MenuItem
+        icon={<MdOutlineDashboard />}
+        label="Dashboard"
+        isActive={true}
+        Link={"/"}
+      />
     </>
   );
 }
